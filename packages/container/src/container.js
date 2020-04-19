c50165f4e17e48f99e5f26e43c910d3ff4b4c958
Radoslaw Mejer <radmen@baethon.com>
2020-04-20 00:29:20 +0200

    Add comments to the Container

--- packages/container/src/container.js
+++ packages/container/src/container.js
@@ -12,10 +12,30 @@ class Container {
     this.bindings = {}
   }
 
+  /**
+   * Bind a factory to a given name
+   *
+   * The factory will be used when resolving value using `make()`.
+   * Note that the `factoryFn` doesn't have to be a function. It might be an object, or a plain value.
+   * Selfwired objects will resolve using the callback passed to the `selfwire()` decorator.
+   *
+   * @param {String} name
+   * @param {Function|Object|*} factoryFn
+   */
   bind (name, factoryFn) {
     this.bindings[name] = utils.resolveFactoryFn(factoryFn)
   }
 
+  /**
+   * Attempt to resolve the value bound to given `name`
+   *
+   * Method will try to autobind any local file paths passed.
+   * To autobind local file prefix the name with `~`.
+   *
+   * @param {String} name
+   * @return {*}
+   * @throws Error when unable to resolve the binding
+   */
   make (name) {
     this._autoBind(name)
 
@@ -26,10 +46,27 @@ class Container {
     throw new Error('Unknown binding: ', name)
   }
 
-  instance (name, object) {
-    this.bind(name, utils.always(object))
+  /**
+   * Bind already resolved value
+   *
+   * This method can be used to store config values,
+   * or objects created outside the container.
+   *
+   * @param {String} name
+   * @param {*} value
+   */
+  instance (name, value) {
+    this.bind(name, utils.always(value))
   }
 
+  /**
+   * Bind a factory to a given name
+   *
+   * Unlike `bind()` this method will resolve the factory and cache the results.
+   *
+   * @param {String} name
+   * @param {Function|Object|*} factoryFn
+   */
   singleton (name, factoryFn) {
     this.bind(name, pipe(
       utils.resolveFactoryFn(factoryFn),
@@ -37,6 +74,12 @@ class Container {
     ))
   }
 
+  /**
+   * Extend the bounded value
+   *
+   * @param {String} name
+   * @param {Function} extendFn
+   */
   extend (name, extendFn) {
     this._autoBind(name)
 
@@ -57,9 +100,11 @@ class Container {
       return
     }
 
-    const pathToResolve = (name.charAt(0) === '~')
-      ? path.join(this.options.root, name.substring(1))
-      : name
+    if (name.charAt(0) !== '~') {
+      throw new Error('_autoBind() works only with local files')
+    }
+
+    const pathToResolve = path.join(this.options.root, name.substring(1))
 
     this.bind(name, require(pathToResolve))
   }
